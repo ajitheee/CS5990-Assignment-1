9c89effb3e0489b54813d43d6f8f66c4b45101bd
ajitheee <119505448+ajitheee@users.noreply.github.com>
2025-02-12 14:14:46 -0800

    Add files via upload

--- similarity.py
+++ similarity.py
@@ -1,6 +1,6 @@
 # -------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
+# AUTHOR: Ajith Elumalai
+# FILENAME: Similarity
 # SPECIFICATION: description of the program
 # FOR: CS 5990 (Advanced Data Mining) - Assignment #1
 # TIME SPENT: how long it took you to complete the assignment
